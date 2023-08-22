e391c9f621b4403207084a0bc91aba829e80b63a
candec <omnius.candec@gmail.com>
2023-08-22 19:55:31 +0100

    everthing working

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,24 +1,29 @@
 #!bin/sh
 
+# Checks if mysql is running by cheking if there is a database called mysql
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
+        # Gives the own rights to mysql group
         chown -R mysql:mysql /var/lib/mysql
 
         # init database
         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
+        # Checks if mktemp directory exists
         tfile=`mktemp`
         if [ ! -f "$tfile" ]; then
                 return 1
         fi
 fi
 
+# Checks if there is a wordpress database, and creates it in case it doesn't.
+# It also add the users root and {user} to the wordpress DB group
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
         cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
