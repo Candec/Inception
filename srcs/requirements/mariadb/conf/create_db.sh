7389a769875a6934f06af2f83ebd97259d5abd9f
candec <omnius.candec@gmail.com>
2023-08-24 17:35:24 +0100

    Adding mdb password

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -9,35 +9,10 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         # init database
         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-        # Checks if mktemp directory exists
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
 fi
 
-# Checks if there is a wordpress database, and creates it in case it doesn't.
-# It also add the users root and {user} to the wordpress DB group
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+cat tmpl.sql | envsubst > /tmp/create_db.sql
 
-        cat << EOF > /tmp/create_db.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci ;
-
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
-#DROP TABLE WHERE '%test%'
--- set root password
-UPDATE mysql.user SET password=password('$DB_ROOT') WHERE user='root';
-
-FLUSH PRIVILEGES;
-EOF
-        # run init.sql
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
-fi
+# run init.sql daemon
+/usr/bin/mysqld --user=mysql --datadir="/var/lib/mysql" --port=3306 --init-file /tmp/create_db.sql $@
+rm -f /tmp/create_db.sql
