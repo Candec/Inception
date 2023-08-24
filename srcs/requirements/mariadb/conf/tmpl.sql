ad3ac54a171c2231da2e9e441d3a6cf6c3887a56
candec <omnius.candec@gmail.com>
2023-08-24 19:15:36 +0100

    args instead of enviroment

--- srcs/requirements/mariadb/conf/tmpl.sql
+++ srcs/requirements/mariadb/conf/tmpl.sql
@@ -4,7 +4,7 @@ CREATE USER IF NOT EXISTS '$MDB_USER_USR'@'%' IDENTIFIED BY '$MDB_USER_PWD';
 GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MDB_USER_USR'@'%' WITH GRANT OPTION;
 
 -- Drop test table
-#DROP TABLE WHERE '%test%'
+-- DROP TABLE WHERE '%test%'
 
 -- set root password
 UPDATE mysql.user SET password=password('$MDB_ROOT_PWD') WHERE user='root';
