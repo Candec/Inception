3c38279fd6a7b043bb3fcd06f333383bb75e30e7
candec <omnius.candec@gmail.com>
2023-08-24 18:03:00 +0100

    args instead of enviroment

---
+++ srcs/requirements/mariadb/conf/tmpl.sql
@@ -0,0 +1,13 @@
+
+-- Create user for the login, set the password and grant the privilages
+CREATE USER IF NOT EXISTS '$MDB_USER_USR'@'%' IDENTIFIED BY '$MDB_USER_PWD';
+GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MDB_USER_USR'@'%' WITH GRANT OPTION;
+
+-- Drop test table
+#DROP TABLE WHERE '%test%'
+
+-- set root password
+UPDATE mysql.user SET password=password('$MDB_ROOT_PWD') WHERE user='root';
+
+-- reset to take effect
+FLUSH PRIVILEGES;
