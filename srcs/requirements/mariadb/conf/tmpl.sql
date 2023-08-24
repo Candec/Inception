fbdb60d011af317e65f788f5b5d13e4d2543112f
candec <omnius.candec@gmail.com>
2023-08-24 20:13:30 +0100

    args instead of enviroment

--- srcs/requirements/mariadb/conf/tmpl.sql
+++ srcs/requirements/mariadb/conf/tmpl.sql
@@ -1,13 +1,12 @@
 
 -- Create user for the login, set the password and grant the privilages
+CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS '$MDB_USER_USR'@'%' IDENTIFIED BY '$MDB_USER_PWD';
 GRANT ALL PRIVILEGES ON wordpress.* TO '$MDB_USER_USR'@'%';
 
 -- Drop test table
 -- DROP TABLE WHERE '%test%'
 
--- set root password
-UPDATE mysql.user SET password=password('$MDB_ROOT_PWD') WHERE user='root';
-
 -- reset to take effect
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
