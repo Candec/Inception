be85e52e9af07006a0af9fcbb77597271fd7f259
candec <omnius.candec@gmail.com>
2023-08-25 23:58:28 +0100

    qb

--- srcs/requirements/mariadb/tools/tmpl.sql
+++ srcs/requirements/mariadb/tools/tmpl.sql
@@ -2,7 +2,7 @@
 -- Create user for the login, set the password and grant the privilages
 CREATE DATABASE IF NOT EXISTS '$MDB_DB_NAME';
 CREATE USER IF NOT EXISTS '$MDB_USER_USR'@'%' IDENTIFIED BY '$MDB_USER_PWD';
-GRANT ALL PRIVILEGES ON '$MDB_DB_NAME'.* TO '$MDB_USER_USR'@'%';
+GRANT ALL PRIVILEGES ON $MDB_DB_NAME.* TO '$MDB_USER_USR'@'%';
 
 -- Drop test table
 -- DROP TABLE WHERE '%test%'
