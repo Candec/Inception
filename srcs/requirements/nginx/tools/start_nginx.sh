0a19705f1d1ff153c370760e8c2018f7d39dc40d
candec <omnius.candec@gmail.com>
2023-08-25 13:52:14 +0100

    qb

--- srcs/requirements/nginx/tools/start_nginx.sh
+++ srcs/requirements/nginx/tools/start_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=EN/ST=Lisbon/L=Lisbon/O=wordpress/CN=jibanez-.42.fr";
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=EN/ST=London/L=London/O=wordpress/CN=jibanez-.42.fr";
 fi
 
 exec "$@"
