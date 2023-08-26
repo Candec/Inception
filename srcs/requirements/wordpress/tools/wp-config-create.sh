869062eafdaa64b500d161b395502ca23d50efab
candec <omnius.candec@gmail.com>
2023-08-26 18:31:04 +0100

    moving wp-config.php

--- srcs/requirements/wordpress/tools/wp-config-create.sh
+++ srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -4,20 +4,18 @@ set -x
 
 cd /var/www/html/wordpress
 
-env
-
 if ! wp core is-installed --allow-root; then
 	# create admin
-	wp core install --allow-root \
-	--url=https://$DOMAIN_NAME \
-	--title=$WP_TITLE \
-	--admin_user=$WP_ADMIN_USER \
-	--admin_password=$WP_ADMIN_PASSWORD \
-	--admin_email=$WP_ADMIN_EMAIL \
+	# wp core install --allow-root \
+	# --url=https://$DOMAIN_NAME \
+	# --title=$WP_TITLE \
+	# --admin_user=$WP_ADMIN_USER \
+	# --admin_password=$WP_ADMIN_PASSWORD \
+	# --admin_email=$WP_ADMIN_EMAIL \
 	#create user
-	wp user create --allow-root \
-		$WP_USER_LOGIN \
-		$WP_USER_EMAIL \
-		--role=author \
-		--user_pass=$WP_USER_PASS
+	# wp user create --allow-root \
+	# 	$WP_USER_LOGIN \
+	# 	$WP_USER_EMAIL \
+	# 	--role=author \
+	# 	--user_pass=$WP_USER_PASS
 fi
