f69b84e6f93f7fb99d446fc3a1d50d172282ef8c
candec <omnius.candec@gmail.com>
2023-08-26 12:06:33 +0100

    qb

--- srcs/requirements/mariadb/tools/tmpl.sql
+++ srcs/requirements/mariadb/tools/tmpl.sql
@@ -1,27 +1,8 @@
+-- https://mariadb.com/kb/en/
 
--- -- Create user for the login, set the password and grant the privilages
--- CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
--- CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PASS';
--- GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
-
--- -- Drop test table
--- -- DROP TABLE WHERE '%test%'
-
--- -- reset to take effect
--- FLUSH PRIVILEGES;
--- UPDATE mysql.user SET password=password('$MARIADB_ROOT_PASS') WHERE user='root';
-
-FLUSH PRIVILEGES;
-
-DELETE FROM	mysql.user WHERE User='';
--- DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASS';
-
-CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED by '$MARIADB_USER_PASS';
-GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MARIADB_USER_NAME'@'%';
-
-FLUSH PRIVILEGES;
+-- create `LOGIN` user, change password and grant privileges to wordpress database
+CREATE USER IF NOT EXISTS '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PASS';
+GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MARIADB_USER_NAME'@'%' WITH GRANT OPTION;
+#DROP TABLE WHERE '%test%'
+-- set root password
+UPDATE mysql.user SET password=password('$MARIADB_ROOT_PASS') WHERE user='root';
