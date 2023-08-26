bb0be1313151ead1a61279dfe80de7b4e33e6dd8
candec <omnius.candec@gmail.com>
2023-08-26 17:51:41 +0100

    change vars at wp-config and www.conf

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', getenv('$DB_USER') );
 define( 'DB_PASSWORD', getenv('$DB_PASS') );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('$HOSTNAME') );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
