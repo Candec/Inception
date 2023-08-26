83448f8a54f24f2dc7da8bcff0ef02c15c0180f6
candec <omnius.candec@gmail.com>
2023-08-26 21:26:01 +0100

    modified mariadb config and script

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -10,9 +10,6 @@ if [ ! -f "/var/www/html/index.html" ]; then
     # static website
     mv /tmp/index.html /var/www/html/index.html
 
-    # adminer
-    wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
-
     wp core download --allow-root
     wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
