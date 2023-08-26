cc0b983d0d1c913ee2433074b3260b85426f7d17
candec <omnius.candec@gmail.com>
2023-08-26 17:33:54 +0100

    checking env

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('MYSQL_DATABASE') );
+define( 'DB_NAME', $DB_NAME );
 
 /** Database username */
-define( 'DB_USER', getenv('MYSQL_USER') );
+define( 'DB_USER', $DB_USER );
 
-/** Database password */
-define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
+/** DaStabase password */
+define( 'DB_PASSWORD', $DB_PASS );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('MYSQL_DB_HOST') );
+define( 'DB_HOST', $HOSTNAME );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
