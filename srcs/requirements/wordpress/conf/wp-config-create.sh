f88a92de1646076b466c7499c277c6b1db951a67
candec <omnius.candec@gmail.com>
2023-08-26 14:02:48 +0100

    Trying to configure wp from code

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -21,3 +21,21 @@ define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
 EOF
 fi
+
+cd /var/www/html/wordpress
+
+if ! wp core is-installed --allow-root; then
+	# create admin
+	wp core install --allow-root \
+	--url=https://$DOMAIN_NAME \
+	--title=$WP_TITLE \
+	--admin_user=$ADMIN_USER \
+	--admin_password=$ADMIN_PASSWORD \
+	--admin_email=$ADMIN_EMAIL \
+	#create user
+	wp user create --allow-root \
+		$WP_USER_LOGIN \
+		$WP_USER_EMAIL \
+		--role=author \
+		--user_pass=$WP_USER_PASS
+fi
