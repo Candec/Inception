83fd48286d8d1ecb5b9ee7a7f14f1dc10140b745
candec <omnius.candec@gmail.com>
2023-08-27 11:36:59 +0100

    tried adding phar to the packages

--- srcs/requirements/wordpress/tools/wp-create-users.sh
+++ srcs/requirements/wordpress/tools/wp-create-users.sh
@@ -7,18 +7,18 @@ wp config create --dbname=$DB_NAME --dbuser=$DB_USER --prompt=$DB_PASS --dbhost=
 wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user="master" --admin_password="Chief" --admin_email="master@chief.com" --skip-email --allow-root
 wp user create candec candec@gmail.com --role=author --user_pass="abc123" --allow-root
 
-if ! wp core is-installed --allow-root; then
-	# create admin
-	wp core install --allow-root \
-	--url=https://$DOMAIN_NAME \
-	--title=Inception \
-	--admin_user=jesus \
-	--admin_password=AbcHOLA_2 \
-	--admin_email=jesus@gmail.com \
-	#create user
-	wp user create --allow-root \
-		candec \
-		candec@gmail.com \
-		--role=author \
-		--user_pass=AbcHOLA_2
-fi
+# if ! wp core is-installed --allow-root; then
+# 	# create admin
+# 	wp core install --allow-root \
+# 	--url=https://$DOMAIN_NAME \
+# 	--title=Inception \
+# 	--admin_user=jesus \
+# 	--admin_password=AbcHOLA_2 \
+# 	--admin_email=jesus@gmail.com \
+# 	#create user
+# 	wp user create --allow-root \
+# 		candec \
+# 		candec@gmail.com \
+# 		--role=author \
+# 		--user_pass=AbcHOLA_2
+# fi
