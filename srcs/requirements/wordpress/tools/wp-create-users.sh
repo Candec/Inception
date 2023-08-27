bcb890476e16864b217181943f35e3b3c8e447cb
candec <omnius.candec@gmail.com>
2023-08-27 13:14:53 +0100

    finding etc php path

--- srcs/requirements/wordpress/tools/wp-create-users.sh
+++ srcs/requirements/wordpress/tools/wp-create-users.sh
@@ -2,8 +2,17 @@ set -x
 
 # cd /var/www/html/wordpress
 
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+chown -R www-data:www-data /var/www/*;
+chown -R 755 /var/www/*;
+mkdir -p /run/php/;
+touch /run/php/php7.3-fpm.pid;
+
 wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --prompt=$DB_PASS --dbhost="mariadb" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+# wp config create --dbname=$DB_NAME --dbuser=$DB_USER --prompt=$DB_PASS --dbhost="mariadb" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+
+mv /var/www/wp-config.php /var/www/wordpress
+
 wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user="master" --admin_password="chief" --admin_email="master@chief.com" --skip-email --allow-root
 wp user create candec candec@gmail.com --role=author --user_pass="abc123" --allow-root
 
