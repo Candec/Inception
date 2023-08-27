084d2b3a98bd41d7d61bda14d7772fb8e22c2e72
candec <omnius.candec@gmail.com>
2023-08-27 02:03:05 +0100

    modified wp packages

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -20,11 +20,11 @@ if [ ! -f "/var/www/html/index.html" ]; then
 
 fi
 
-sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/zlib.output_compression = .*/zlib.output_compression = on/" /etc/php/7.3/fpm/php.ini
-sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php7/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php7/fpm/php.ini
+sed -i "s/zlib.output_compression = .*/zlib.output_compression = on/" /etc/php7/fpm/php.ini
+sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php7/fpm/php.ini
 
-service php7.3-fpm start
-service php7.3-fpm stop
-php-fpm7.3 -F -R
+service php7-fpm start
+service php7-fpm stop
+php-fpm7 -F -R
