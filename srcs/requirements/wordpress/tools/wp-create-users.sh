c09610fdc62228ea3e3537a8b73e586521345f19
candec <omnius.candec@gmail.com>
2023-08-27 13:23:15 +0100

    finding etc php path

--- srcs/requirements/wordpress/tools/wp-create-users.sh
+++ srcs/requirements/wordpress/tools/wp-create-users.sh
@@ -2,19 +2,12 @@ set -x
 
 # cd /var/www/html/wordpress
 
-# sed -i "s/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/" "/etc/php/8.2/fpm/pool.d/www.conf";
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
-mkdir -p /run/php/;
-touch /run/php/php8.2-fpm.pid;
-
 wp core download --allow-root
 # wp config create --dbname=$DB_NAME --dbuser=$DB_USER --prompt=$DB_PASS --dbhost="mariadb" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+mv /wp-config-php .
 
-# mv /var/www/wp-config.php /var/www/wordpress
-
-# wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user="master" --admin_password="chief" --admin_email="master@chief.com" --skip-email --allow-root
-# wp user create candec candec@gmail.com --role=author --user_pass="abc123" --allow-root
+wp core install --url=$DOMAIN_NAME/wordpress --title="Inception" --admin_user="master" --admin_password="chief" --admin_email="master@chief.com" --skip-email --allow-root
+wp user create candec candec@gmail.com --role=author --user_pass="abc123" --allow-root
 
 # if ! wp core is-installed --allow-root; then
 # 	# create admin
